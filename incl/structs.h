b35a7523f8524f2cab9614c0eae2a9d17b2b5bb2
Laura de Koning <lade-kon@student.codam.nl>
2024-07-24 18:01:47 +0200

    finished enums

--- incl/structs.h
+++ incl/structs.h
@@ -6,7 +6,7 @@
 /*   By: lade-kon <lade-kon@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/06/12 19:12:56 by lade-kon      #+#    #+#                 */
-/*   Updated: 2024/07/24 16:30:58 by lade-kon      ########   odam.nl         */
+/*   Updated: 2024/07/24 18:00:55 by lade-kon      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,30 +17,19 @@
 
 typedef enum e_type
 {
-	T_COMMAND,
-	T_BUILTIN,
-	T_FLAG,
-	T_FILE,
-	T_STRING,
-	T_PIPE,
-	T_REDIRECT,
-	T_ENV_VARIABLE,
-	T_LAST_EXIT,
-	T_TYPE_NOT_FOUND,
-	T_EOF,
-	T_UNKNOWN
-}	t_type;
+	TEXT,
+	PIPE,
+	IN_REDIRECT,
+	OUT_REDIRECT,
+	APP_REDIRECT,
+	HEREDOC
+}			t_type;
 
 typedef struct s_token
 {
 	char			*value;
 	t_type			type;
+	struct s_token	*next;
 }					t_token;
-
-typedef struct s_output
-{
-	t_token			token;
-	struct s_output	*next;
-}					t_output;
 
 #endif
