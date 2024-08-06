4c41cf4a3aea6749ea9dede14b729f79d6956ef6
Milan Nijsen <mnijsen@f1r5s14.codam.nl>
2024-08-06 16:02:47 +0200

    some clean up of comments, consolidation of files, leaks prevented, working version.

--- minishell.h
+++ minishell.h
@@ -5,66 +5,45 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <errno.h>
 #include <stdbool.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include "built_enviroment/libft.h"
+#include "structs.h"
 #include <linux/limits.h>
+#include <string.h>
+#include <signal.h>
 
-typedef struct s_output
-{
-	char			*node;
-	int				type_node;
-	struct s_output	*next_node;
-}	t_output;
 
-typedef struct s_env
-{
-	char			*name;
-	char			*content;
-	struct s_env	*next_node;
-}	t_env;
-
-typedef struct s_heredoc
-{
-	int					pipe_ids[2];
-	unsigned int		index;//maybe not needed if you just move thru it as you encounter heredocs during the creating of pipes otherwise anytime you encounter a heredoc you need to check how many heredocs there were before hand
-	struct s_heredoc	*next_node;
-}	t_heredoc;
-
-//also include env variables
-//might aswell also include envp
-#define MINI_COMMAND 1 //is the first in a section so doesnt matter
-#define MINI_BUILTIN 2 //is the first in a section so doesnt matter can replace if we make a is builtin func
-#define MINI_FLAG 3 //does not matter
-#define MINI_FILE 4 //does not matter
-#define MINI_STRING 5 //does not matter
-#define MINI_PIPE 6
-#define MINI_REDIRECT 7
-#define MINI_ENV_VARIABLE 8 //expand before execute
-#define MINI_LAST_EXIT 9 //can be converted to a builtin
-#define MINI_TYPE_NOT_FOUND 0
 
 t_env *array_to_list(char **envp);
 void	free_env(t_env	*node);
-char **list_to_array(t_env *node);
+char **list_to_env(t_env *node);
+char **tokens_to_args(t_token *node);
 int	modify_env(t_env *node, char *name, char *content, bool heap);
 int	remove_env(t_env *node, char *name);
 char *return_env(t_env *node, char *name);
+int add_env(t_env *node, char *name, char *content, bool heap);
 
-int bi_cd(char **args, t_env *env_node);
-int	bi_echo(char **args);
+int bi_cd(const t_token *token, t_env *env_node);
+int	bi_echo(const t_token *token);
 int bi_env(t_env *env_node);
 int bi_exit(bool is_child);
-int bi_export(char **args, t_env *env_node);
+int bi_export(const t_token *token, t_env *env_node);
 int	split_env(char *str, char **output1, char **output2);
 int bi_pwd(t_env *env_node);
-int	bi_unset(char **name, t_env *env_node);
+int	bi_unset(const t_token *token, t_env *env_node);
 t_env *creat_env(char *name, char *content);
 char *return_pwd(t_env *env_node);
 int pipeline(void);
-
+t_token *redirect(t_token *first_token, t_heredoc *h_doc, int h_index, int *exit_code);
+void	free_token(t_token *token);
+void	free_tokens(t_token *token);
+void	save_std_fd(void);
+void	reset_std_fd(void);
+t_token *heredoc(t_token *token, t_heredoc *h_node, unsigned int index, int *exit_code);
 
 
 /*define common return codes mostly so malloc failure shuts the program down and isnt treated like other error codes like syntax error*/
@@ -72,8 +51,8 @@ int pipeline(void);
 #define SUCCES 0
 #define COMMON_FAILURE 1
 
-#define LOCAL 0
-#define EXPORT 1
+#define STDIN_CLONE 3
+#define STDOUT_CLONE 4
 
 #define ENV_VARIABLE_LENGTH 96
 
---
+++ structs.h
@@ -0,0 +1,52 @@
+#ifndef STRUCTS_H
+# define STRUCTS_H
+
+# include "minishell.h"
+/** 
+* @brief The different enum types for the tokens
+*
+* @param TEXT is a string
+* @param PIPE (|)
+* @param IN_REDIRECT (>)
+* @param OUT_REDIRECT (<)
+* @param APP_REDIRECT (>>)
+* @param HEREDOC (<<)
+*/
+typedef enum e_type
+{
+	TEXT,
+	PIPE,
+	IN_REDIRECT,
+	OUT_REDIRECT,
+	APP_REDIRECT,
+	HEREDOC
+}			t_type;
+
+typedef struct s_token
+{
+	char			*value;
+	t_type			type;
+	struct s_token	*next;
+}					t_token; // cat -e
+
+typedef struct s_env
+{
+	char			*name;
+	char			*content;
+	struct s_env	*next_node;
+}	t_env;
+
+typedef struct s_heredoc
+{
+	int					pipe_ids[2];
+	unsigned int		index;//based on section
+	struct s_heredoc	*next_node;
+}	t_heredoc;
+
+typedef struct s_exec
+{
+	int		index;
+	bool	pipe_present;
+}	t_exec;
+
+#endif
