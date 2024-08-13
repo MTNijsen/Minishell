305c04069d107309d63b35d82ea0ce1b31fd430d
Milan Nijsen <mnijsen@f1r5s13.codam.nl>
2024-08-13 11:45:27 +0200

    header and notes updates

--- minishell.h
+++ minishell.h
@@ -18,39 +18,31 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+int executor(t_data *data);
 
-t_env *array_to_list(char **envp);
-void	free_env(t_env	*node);
-char **list_to_env(t_env *node);
-char **tokens_to_args(t_token *node);
-int	modify_env(t_env *node, char *name, char *content, bool heap);
-int	remove_env(t_env *node, char *name);
-char *return_env(t_env *node, char *name);
-int add_env(t_env *node, char *name, char *content, bool heap);
+int		bi_cd(char **argv, t_data *data);
+void	bi_echo(char **argv);
+void	bi_env(t_data *data);
+void	bi_exit(char **argv, t_data *data, bool pipe_present);
+void	bi_export(char **argv, t_data *data);
+int		bi_pwd(t_data *data);
+void	bi_unset(char **argv, t_data *data);
 
-int bi_cd(const t_token *token, t_env *env_node);
-int	bi_echo(const t_token *token);
-int bi_env(t_env *env_node);
-int bi_exit(bool is_child);
-int bi_export(const t_token *token, t_env *env_node);
-int	split_env(char *str, char **output1, char **output2);
-int bi_pwd(t_env *env_node);
-int	bi_unset(const t_token *token, t_env *env_node);
-t_env *creat_env(char *name, char *content);
-char *return_pwd(t_env *env_node);
+bool	is_valid_env(char *env_var);
+char	**realloc_envp(char **envp, size_t size, size_t *old_size);
+int		modify_env_var(t_data *data, char *env_var);
+void	remove_env_var(t_data *data, char *name);
+char	*return_env_val(char **envp, char *name);
+
+
+
 int pipeline(void);
-t_token *redirect(t_token *first_token, t_heredoc *h_doc, int h_index, int *exit_code);
-void	free_token(t_token *token);
-void	free_tokens(t_token *token);
-int executor(t_token *token, t_env *env_node);
-int command(t_token *node, t_env *env_node, bool pipe_present , int *pid);
-int builtin(t_token *node, t_env *env_node, bool is_child);
-int isbuiltin(char *str);
-void	free_heredoc(t_heredoc *node);
 void wait_exit(int pid, int *exit_code);
-t_token *heredoc_receive(t_token *token, t_heredoc *h_doc, unsigned int index, int *exit_code);
-t_heredoc *heredoc_read(t_token *token, int *exit_code);
+int	redirect(t_proc *proc);
+int heredoc(t_data *data);
+void clean_exit(t_data *data, int exit_status);
 
+char *return_pwd(t_data *data);
 
 /*define common return codes mostly so malloc failure shuts the program down and isnt treated like other error codes like syntax error*/
 #define MALLOC_FAILURE -1
--- structs.h
+++ structs.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   structs.h                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lade-kon <lade-kon@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/06/12 19:12:56 by lade-kon      #+#    #+#                 */
+/*   Updated: 2024/08/09 15:02:13 by mnijsen       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef STRUCTS_H
 # define STRUCTS_H
 
@@ -5,12 +17,14 @@
 /** 
 * @brief The different enum types for the tokens
 *
-* @param TEXT is a string
+* @param CMD is a command
 * @param PIPE (|)
 * @param IN_REDIRECT (>)
 * @param OUT_REDIRECT (<)
 * @param APP_REDIRECT (>>)
 * @param HEREDOC (<<)
+* @param FILE is the in or out file
+* @param STRING is a string
 */
 typedef enum e_type
 {
@@ -19,15 +33,25 @@ typedef enum e_type
 	IN_REDIRECT,
 	OUT_REDIRECT,
 	APP_REDIRECT,
-	HEREDOC
+	HEREDOC,
+	COMMAND,
+	FILES,
+	STRING
 }			t_type;
+
+typedef struct s_token
+{
+	char			*value;
+	t_type			type;
+	struct s_token	*next;
+}					t_token;
 
 typedef struct s_redir
 {
-	t_type	type; // < << > >>
-	char	*file; // test.txt, EOF
+	t_type			type; // < << > >>
+	char			*file; // test.txt, EOF
 	struct s_redir	*next;
-}	t_redir;
+}					t_redir;
 
 /**
  * @brief	This struct will be a node in a list of processes. It's an organized
@@ -46,7 +70,6 @@ typedef struct s_redir
  * @param	token_count: amount of tokens in process.
  * @param	index: index of which number proces this is. starting with 0.
  */
-
 typedef struct s_proc
 {
 	char			*cmd;
@@ -54,8 +77,22 @@ typedef struct s_proc
 	t_redir			*redir;
 	int				hd_pipe[2];
 	int				token_count;
+	int				proc_count;
 	int				index;
 	struct s_proc	*next;
 }					t_proc;
+/**
+ * @brief	This is the giant struct with all of the data from the current prompt
+ * 
+ * 
+ */
+typedef struct s_data
+{
+	t_proc	*procs;
+	t_token	*tokens;
+	char	**envp;
+	size_t	env_count;
+	char	*input;
+}			t_data;
 
 #endif
