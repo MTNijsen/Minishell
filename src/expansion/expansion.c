50a3cad37a68b428313ef2fd8855d06c23aa02b7
Laura de Koning <laura-dekoning@live.nl>
2024-09-17 11:22:54 +0200

    Worked on expansion

--- src/expansion/expansion.c
+++ src/expansion/expansion.c
@@ -6,31 +6,48 @@
 /*   By: lade-kon <lade-kon@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/06/12 12:31:35 by lade-kon      #+#    #+#                 */
-/*   Updated: 2024/09/04 19:24:52 by lade-kon      ########   odam.nl         */
+/*   Updated: 2024/09/12 13:22:05 by lade-kon      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	expand(t_token *token)
-{
-	char	*to_expand;
-	t_token	*current;
-	int		i;
-	int		len;
+	// char	*to_expand;
+	// int		i;
+	// int		len;
 
-	i = 0;
-	current = token;
-	if (ft_strcmp(current->value, "$") == 0)
+	// if (ft_strcmp(current->next->value, "$") == 0)
+	// 	to_delete = current->next;
+
+	// to_expand = current->next->value;
+	// len = ft_strlen(to_expand);
+	// if (to_expand[0] == '\"')
+	// {
+	// 	to_expand = ft_substr(to_expand, 1, (len - 2));
+	// 	printf("%s\n", to_expand);
+	// 	// delete $ en text token en add expanded tokens.
+	// }
+
+
+void	delete_token(t_token **head, t_token *to_delete)
+{
+	t_token	*current;
+	t_token *temp;
+	if (*head == to_delete)
 	{
-		to_expand = current->next->value;
-		len = ft_strlen(to_expand);
-		if (to_expand[0] == '\"')
-		{
-			to_expand = ft_substr(to_expand, 1, (len - 2));
-			printf("%s\n", to_expand);
-			// delete $ en text token en add expanded tokens.
-		}
+		temp = *head;
+		*head = (*head)->next;
+		free(temp->value);
+		free(temp);
+	}
+	current = *head;
+	while (current != NULL && current->next != to_delete)
+		current = current->next;
+	if (current != NULL && current->next == to_delete)
+	{
+		current->next = to_delete->next;
+		free(to_delete->value);
+		free(to_delete);
 	}
 }
 
