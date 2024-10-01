d6b54a81b658dca8011b11fba452a45843cc4708
Laura de Koning <laura-dekoning@live.nl>
2024-10-01 20:55:17 +0200

    worked on removing quotes

--- src/expansion/handle_quotes.c
+++ src/expansion/handle_quotes.c
@@ -6,16 +6,11 @@
 /*   By: lade-kon <lade-kon@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/06/12 12:08:08 by lade-kon      #+#    #+#                 */
-/*   Updated: 2024/10/01 16:27:51 by lade-kon      ########   odam.nl         */
+/*   Updated: 2024/10/01 20:54:18 by lade-kon      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-void	remove_quotes(char *str)
-{
-
-}
 
 int	check_spaces(char *str)
 {
@@ -57,6 +52,62 @@ int	check_quotes(char *str)
 		i++;
 	}
 	return (0);
+}
+int	type_quote(int q)
+{
+	if (q == "\"")
+		return ("\"")
+	else if (q = "\'")
+		return ("\'")
+	return (0);
+}
+
+void	no_quote_cpy_loop(char *str, char *new, int *i, int *j, int q)
+{
+	if (q = 0)
+	{
+		while (str[(*i)] && !is_quote(str[(*i)]))
+		{
+			new[(*j)] = str[(*i)];
+			(*i)++;
+			(*j)++;
+		}
+	}
+	else
+	{
+		while (str[(*i)] && str[(*i)]  != q)
+		{
+			new[(*j)] = str[(*i)];
+			(*i)++;
+			(*j)++;
+		}
+	}
+}
+
+void	remove_quotes(char *str)
+{
+	char	*new;
+	int		q;
+	int		i;
+	int		j;
+
+	new = (char *)ft_calloc(sizeof(char), (ft_strlen(str) - 1));
+	i = 0;
+	j = 0;
+	q = 0;
+	while (str[i])
+	{
+		no_quote_cpy_loop(str, new, &i, &j, q)
+		if (is_quote(str[i]))
+		{
+			q = str[i];
+			while ()
+		}
+		
+	}
+
+	q = type_quote(str[i]);
+
 }
 
 void	handle_quotes(t_data *data)
