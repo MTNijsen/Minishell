403ba71df56c8030d70ad13b0f82a4ced1bc24eb
Laura de Koning <laura-dekoning@live.nl>
2024-10-01 16:05:17 +0200

    deleted create_quote_tokens. Now the create_text_tokens will just create a token including all of the quotes. So lexing only seperates with a space

--- src/lexer/text_tokens.c
+++ src/lexer/text_tokens.c
@@ -6,13 +6,13 @@
 /*   By: lade-kon <lade-kon@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/08/01 21:27:34 by lade-kon      #+#    #+#                 */
-/*   Updated: 2024/09/05 18:34:34 by lade-kon      ########   odam.nl         */
+/*   Updated: 2024/10/01 16:04:07 by lade-kon      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	create_text_token(t_data *data, char *input, int i)
+int	create_text_token(t_data *data, char *input, int i)
 {
 	t_token	*token;
 	int		start;
@@ -29,36 +29,4 @@ static int	create_text_token(t_data *data, char *input, int i)
 	add_token(data, token);
 	free (value);
 	return (i);
-}
-
-static int	create_quotes_token(t_data *data, char *input, int i)
-{
-	t_token	*token;
-	int		x;
-	int		start;
-	char	*value;
-
-	start = i;
-	value = NULL;
-	x = handle_quotes(input, i);
-	value = ft_substr(input, (start), (x - start));
-	i = x;
-	token = create_token(STRING, value);
-	if (token == NULL || value == NULL)
-		return (-1);
-	add_token(data, token);
-	free (value);
-	return (i);
-}
-
-int	text_tokens(t_data *data, char *input, int i)
-{
-	int	x;
-
-	x = 0;
-	if (input[i] == '\"' || input[i] == '\'')
-		x = create_quotes_token(data, input, i);
-	else
-		x = create_text_token(data, input, i);
-	return (x);
 }
